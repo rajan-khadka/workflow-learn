1cf72ab304a312c7ed62b3683ae476953b61a11c
rajan.khadka <rajan@noveltytechnology.com>
2024-02-05 17:47:00 +0545

    workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+// import App from './App.tsx'
 import './index.css'
 import NoteState from './context/NoteContext/NoteState.tsx'
 
